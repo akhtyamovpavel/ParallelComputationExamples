6aebb66464c8c03f7e4d9fbe4cc07296fd8c532f
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2020-08-31 18:02:32 +0300

    Added extra commands to sbatch

--- MPI/00-hello-world/bin/run_sbatch_config.sh
+++ MPI/00-hello-world/bin/run_sbatch_config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
 #SBATCH --ntasks=8
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=RT
 #SBATCH --job-name=example
 #SBATCH --comment="Run mpi from config"
+#SBATCH --output=out.txt
+#SBATCH --error=error.txt
 mpiexec ./MpiHelloWorld
