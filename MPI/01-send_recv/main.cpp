3ef3bc758e4a577e3308c1af771f96ab985c2082
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2020-08-31 17:59:43 +0300

    Converted tabs to spaces

--- MPI/01-send_recv/main.cpp
+++ MPI/01-send_recv/main.cpp
@@ -2,27 +2,27 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-	
-	MPI_Init(&argc, &argv);
+    
+    MPI_Init(&argc, &argv);
 
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	int world_rank;
+    int world_rank;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	
-	int number;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    
+    int number;
 
     int array[10];
 
-	MPI_Status status;
+    MPI_Status status;
     if (world_rank == 0) {
         for (int i = 0; i < 10; ++i) {
             array[i] = i;
         }
 
-		MPI_Send(
+        MPI_Send(
             &array[5] /* pointer to start */,
             5 /* number of words */,
             MPI_INT /* word type */, 
@@ -30,8 +30,8 @@ int main(int argc, char** argv) {
             0 /* tag */,
             MPI_COMM_WORLD /* communicator: default - all world */
         );
-	} else if (world_rank == 1) {
-		MPI_Recv(
+    } else if (world_rank == 1) {
+        MPI_Recv(
             &array[5] /* pointer to start */, 
             5 /* number of words */, 
             MPI_INT /* type of word */, 
@@ -40,15 +40,15 @@ int main(int argc, char** argv) {
             MPI_COMM_WORLD /* communicator: default - all world */,
             &status
         );
-		std::cout << "Process 1 received 5 elements from process 0. They are" << std::endl;
+        std::cout << "Process 1 received 5 elements from process 0. They are" << std::endl;
 
         for (int i = 5; i < 10; ++i) {
             std::cout << array[i] << " ";
         }
         std::cout << std::endl;
-	}
+    }
 
-	
-	MPI_Finalize();
-	return 0;
+    
+    MPI_Finalize();
+    return 0;
 }
