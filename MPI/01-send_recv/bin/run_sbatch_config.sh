e1f6654d6cb1c5e436593142fe73e51f948d48e5
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2024-09-10 07:24:02 +0000

    Fixed example of SendRecv

--- MPI/01-send_recv/bin/run_sbatch_config.sh
+++ MPI/01-send_recv/bin/run_sbatch_config.sh
@@ -2,9 +2,10 @@
 #
 #SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=RT
 #SBATCH --job-name=sendrecvexample
 #SBATCH --comment="Run MPI SendRecv"
 #SBATCH --output=out.txt
 #SBATCH --error=error.txt
+sbcast -f MpiSendRecv $PWD/MpiSendRecv
+ls $PWD
 mpiexec ./MpiSendRecv
